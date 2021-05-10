7dedcd297a0381532e70854805483cd7a3ab241d
Nikolai Gotzmann <49162313+ngotzmann@users.noreply.github.com>
2021-05-10 09:58:36 +0200

    Update Application.java

--- src/main/java/net/ngotzmann/SimpleFileVirusScan/Application.java
+++ src/main/java/net/ngotzmann/SimpleFileVirusScan/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
